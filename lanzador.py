30d98fa1646845cf193011986419871d0b8d58e4
jzazooro <91785177+jzazooro@users.noreply.github.com>
2023-05-18 08:15:41 +0200

    ajustes por que no me va cuando lo hago sin semaforos

--- lanzador.py
+++ lanzador.py
@@ -4,18 +4,23 @@ from threading import Thread
 import time
 
 def main():
-    print("A continuacion se ejecutara el programa con semaforos")
-    print("----------------------------------------------------")
-    time.sleep(1)
-    h1=Thread(target=productor)
-    h2=Thread(target=consumidor)
-    h1.start()
-    h2.start()
 
-    print("A continuacion se ejecutara el programa sin semaforos")
-    print("----------------------------------------------------")
-    time.sleep(1)
-    h3=Thread(target=productorsinsemaforo)
-    h4=Thread(target=consumidorsinsemaforo)
-    h3.start()
-    h4.start()
+    apartado=int(input("Introduzca 1 si lo quiere con semaforos, o introduzca 2 si lo quiere sin semaforos: "))
+
+    if apartado ==1:
+        print("A continuacion se ejecutara el programa con semaforos")
+        print("----------------------------------------------------")
+        time.sleep(1)
+        h1=Thread(target=productor)
+        h2=Thread(target=consumidor)
+        h1.start()
+        h2.start()
+
+    if apartado ==2:
+        print("A continuacion se ejecutara el programa sin semaforos")
+        print("----------------------------------------------------")
+        time.sleep(1)
+        h1=Thread(target=productorsinsemaforo)
+        h2=Thread(target=consumidorsinsemaforo)
+        h1.start()
+        h2.start()
